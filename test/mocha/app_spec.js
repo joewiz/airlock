e514ab07b387c969fbcbbe98fc8fd57ab8191e89
Joe Wicentowski <joewiz@gmail.com>
2026-01-05 19:29:01 -0500

    Fix CI failure by updating glob usage in tests
    update for glob v9+, with help of ChatGPT

--- test/mocha/app_spec.js
+++ test/mocha/app_spec.js
@@ -4,7 +4,7 @@ const chai = require('chai')
 const chaiXml = require('chai-xml')
 const expect = require('chai').expect
 const fs = require('fs-extra')
-const glob = require('glob')
+const { glob } = require('glob')
 const xmldoc = require('xmldoc')
 const assert = require('yeoman-assert')
 
@@ -12,56 +12,46 @@ const assert = require('yeoman-assert')
 describe('file system checks', function () {
   describe('markup files are well-formed', function () {
     chai.use(chaiXml)
-    it('*.html is xhtml', function (done) {
-      glob('**/*.html', { ignore: 'node_modules/**' }, function (err, files) {
-        if (err) throw err
-        // console.log(files)
-        files.forEach(function (html) {
-          const xhtml = fs.readFileSync(html, 'utf8')
-          const hParsed = new xmldoc.XmlDocument(xhtml).toString()
-          expect(hParsed).xml.to.be.valid()
-        })
+
+    it('*.html is xhtml', async function () {
+      const files = await glob('**/*.html', { ignore: 'node_modules/**' })
+      // console.log(files)
+      files.forEach(function (html) {
+        const xhtml = fs.readFileSync(html, 'utf8')
+        const hParsed = new xmldoc.XmlDocument(xhtml).toString()
+        expect(hParsed).xml.to.be.valid()
       })
-      done()
     })
 
-    it('*.xml', function (done) {
-      glob('**/*.xml', { ignore: 'node_modules/**' }, function (err, files) {
-        if (err) throw err
-        // console.log(files)
-        files.forEach(function (xmls) {
-          const xml = fs.readFileSync(xmls, 'utf8')
-          const xParsed = new xmldoc.XmlDocument(xml).toString()
-          expect(xParsed).xml.to.be.valid()
-        })
+    it('*.xml', async function () {
+      const files = await glob('**/*.xml', { ignore: 'node_modules/**' })
+      // console.log(files)
+      files.forEach(function (xmls) {
+        const xml = fs.readFileSync(xmls, 'utf8')
+        const xParsed = new xmldoc.XmlDocument(xml).toString()
+        expect(xParsed).xml.to.be.valid()
       })
-      done()
     })
 
-    it('*.xconf', function (done) {
-      glob('**/*.xconf', { ignore: 'node_modules/**' }, function (err, files) {
-        if (err) throw err
-        // console.log(files)
-        files.forEach(function (xconfs) {
-          const xconf = fs.readFileSync(xconfs, 'utf8')
-          const cParsed = new xmldoc.XmlDocument(xconf).toString()
-          expect(cParsed).xml.to.be.valid()
-        })
+    it('*.xconf', async function () {
+      const files = await glob('**/*.xconf', { ignore: 'node_modules/**' })
+      // console.log(files)
+      files.forEach(function (xconfs) {
+        const xconf = fs.readFileSync(xconfs, 'utf8')
+        const cParsed = new xmldoc.XmlDocument(xconf).toString()
+        expect(cParsed).xml.to.be.valid()
       })
-      done()
     })
-    it('*.odd', function (done) {
+
+    it('*.odd', async function () {
       this.slow(1000)
-      glob('**/*.odd', { ignore: 'node_modules/**' }, function (err, files) {
-        if (err) throw err
-        // console.log(files)
-        files.forEach(function (odds) {
-          const odd = fs.readFileSync(odds, 'utf8')
-          const xParsed = new xmldoc.XmlDocument(odd).toString()
-          expect(xParsed).xml.to.be.valid()
-        })
+      const files = await glob('**/*.odd', { ignore: 'node_modules/**' })
+      // console.log(files)
+      files.forEach(function (odds) {
+        const odd = fs.readFileSync(odds, 'utf8')
+        const xParsed = new xmldoc.XmlDocument(odd).toString()
+        expect(xParsed).xml.to.be.valid()
       })
-      done()
     })
   })
 
